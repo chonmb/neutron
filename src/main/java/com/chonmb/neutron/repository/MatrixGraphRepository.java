5f8454430cb1c1fe4c3f220c13cada0fef5eecbf
chonmb <weichonmb@foxmail.com>
2024-07-25 23:30:55 +0800

    remove matrix test

--- src/main/java/com/chonmb/neutron/repository/MatrixGraphRepository.java
+++ src/main/java/com/chonmb/neutron/repository/MatrixGraphRepository.java
@@ -12,14 +12,14 @@ import java.util.stream.Stream;
 public class MatrixGraphRepository implements EventGraphRepository {
     @Override
     public Optional<EventDeliverMap> getEventGraph(String topic) {
-        if ("line".equals(topic)) {
-            boolean[][] map = {{false, true, false}, {false, false, true}, {false, false, false}};
-            RequirementInfo node1 = new SimpleRequirementInfo("111", "echo");
-            RequirementInfo node2 = new SimpleRequirementInfo("222", "echo");
-            RequirementInfo node3 = new SimpleRequirementInfo("333", "echo");
-            MatrixEventGraph matrixEventGraph = new MatrixEventGraph(map, Stream.of(node1, node2, node3).collect(Collectors.toList()));
-            return Optional.of(matrixEventGraph);
-        }
+//        if ("line".equals(topic)) {
+//            boolean[][] map = {{false, true, false}, {false, false, true}, {false, false, false}};
+//            RequirementInfo node1 = new SimpleRequirementInfo("111", "echo");
+//            RequirementInfo node2 = new SimpleRequirementInfo("222", "echo");
+//            RequirementInfo node3 = new SimpleRequirementInfo("333", "echo");
+//            MatrixEventGraph matrixEventGraph = new MatrixEventGraph(map, Stream.of(node1, node2, node3).collect(Collectors.toList()));
+//            return Optional.of(matrixEventGraph);
+//        }
         return Optional.empty();
     }
 }
