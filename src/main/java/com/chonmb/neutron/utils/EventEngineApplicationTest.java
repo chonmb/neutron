d7b6d1ece49f1a58c219e7139f0c9777012ac877
chonmb <weichonmb@foxmail.com>
2024-07-25 23:28:51 +0800

    update application test

---
+++ src/main/java/com/chonmb/neutron/utils/EventEngineApplicationTest.java
@@ -0,0 +1,13 @@
+package com.chonmb.neutron.utils;
+
+import java.lang.annotation.*;
+
+/**
+ * @author chonmb
+ * @date 2024/7/24 16:06
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface EventEngineApplicationTest {
+}
