8a1ce2cd138be68c8650cd26ad350660b43f90e2
chonmb <weichonmb@foxmail.com>
2024-08-09 00:36:19 +0800

    format log

--- src/main/java/com/chonmb/neutron/requirements/instance/EchoRequirement.java
+++ src/main/java/com/chonmb/neutron/requirements/instance/EchoRequirement.java
@@ -1,13 +1,17 @@
 package com.chonmb.neutron.requirements.instance;
 
 import com.chonmb.neutron.event.Stimulation;
+import com.chonmb.neutron.log.LoggerFactory;
+
+import java.util.logging.Logger;
 
 
 public class EchoRequirement extends AbstractRequirementInstance {
+    private final Logger logger = LoggerFactory.getLogger(EchoRequirement.class);;
 
     @Override
     public void apply(Stimulation stimulation) {
-        System.out.println("echo executor execute stimulation, print content: "+stimulation.content());
+        logger.info("echo executor execute stimulation, print content: "+stimulation.content());
     }
 
     @Override
